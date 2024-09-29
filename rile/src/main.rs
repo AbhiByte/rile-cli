369f7ed236502c2208f5ccbeeb7dcec0e30bd119
Abhinav Ramesh <dbossgames20@gmail.com>
2024-09-28 21:13:26 -0400

    remove default outline

--- rile/src/main.rs
+++ rile/src/main.rs
@@ -4,11 +4,10 @@ use clap::Parser;
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 struct Args {
-    /// Name of the person to greet
     #[arg(short, long)]
-    name: String,
+    file: String,
 
-    /// Number of times to greet
+    
     #[arg(short, long, default_value_t = 1)]
     count: u8,
 }
@@ -16,7 +15,8 @@ struct Args {
 fn main() {
     let args = Args::parse();
 
-    for _ in 0..args.count {
-        println!("Hello {}!", args.name);
-    }
+    
+    println!("The file type is {}!", args.file);
+    
+
 }
